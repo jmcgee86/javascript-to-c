e2f79219f931b6c282643a56022180ee48eec825
Jim McGee <jmcgee86@gmail.com>
2018-04-17 22:53:19 +0000

    factorialize

---
+++ factorialize.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+
+long long factorialize(input)
+{
+    long long num;
+    for (int i = 1; input > 0; input--)
+    {
+        i *= input;
+        num = i;
+    }
+    printf("%lld", num);
+    return num;
+}
+
+int main(int argc, string argv[])
+{
+    int input;
+
+    if (argc ==2)
+    {
+        input = atoi(argv[1]);
+        factorialize(input);
+    }
+
+    return 0;
+
+}
